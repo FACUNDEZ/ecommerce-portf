7619087e670ad30d506e0da7093ee89274473b38
FACUNDEZ <facu.mdq@live.com>
2024-01-05 16:44:15 -0300

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,27 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        translateX: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        translateX0: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        translateY: {
+          '0%': { transform: 'translateY(-150%)' },
+          '100%': { transform: 'translateY(0%)' },
+        }
+      },
+      animation: {
+          translateX: 'translateX 1.5s ease-in-out',
+          translateX0: 'translateX 1.5s ease-in-out',
+          translateY: 'translateY 1.5s ease-in-out',
+        },
+      }
     },
-  },
   plugins: [],
 }
 export default config
